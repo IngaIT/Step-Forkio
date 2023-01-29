57de4831e77671b6875bd52ba9f26c51cf03d294
ShustovD <dima6shustov55236@gmail.com>
2023-01-29 22:26:16 +0200

    add almost full header

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-// const burger = document.querySelector('.burger-menu');
-// const menu = document.querySelector('.header-links');
-
-// function handlerBurger() {
-//     burger.classList.toggle('active');
-//     menu.classList.toggle('active');
-// }
-
-// burger.addEventListener('click', handlerBurger);
